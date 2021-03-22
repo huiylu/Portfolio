22e961c9d43229873395ed9c80b3443c56e26597
huiylu <74116599+huiylu@users.noreply.github.com>
2021-03-22 03:34:39 -0700

    Needs more css word

--- src/component/Projects.js
+++ src/component/Projects.js
@@ -2,14 +2,17 @@ function Projects(){
     return(
         <div className="info-block">
             <h1>Projects</h1>
-            <div>
+            <div className="project">
                 <h4>Spotify-Api-Tester</h4>
+                <p>Description</p>
             </div>
-            <div>
+            <div className="project">
                 <h4>PoG Market Watch</h4>
+                <p>Description</p>
             </div>
-            <div>
+            <div className="project">
                 <h4>Project3</h4>
+                <p>Description</p>
             </div>
         </div>
     )
--- src/component/Skills.js
+++ src/component/Skills.js
@@ -2,17 +2,17 @@ function Skills(){
     return(
         <div className="info-block">
             <h1>Skills</h1>
-            <div>
+            <div className="block">
                 <p>HTML5</p>
                 <p>Javascript</p>
                 <p>Node.js</p>
             </div>
-            <div>
+            <div className="block">
                 <p>REACT</p>
                 <p>Python</p>
                 <p>MongoDB</p>
             </div>
-            <div>
+            <div className="block">
                 <p>SQL</p>
                 <p>C++</p>
                 <p>Java</p>
