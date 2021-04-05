63f9debc881e1fe09972a4180216c4f8c38f839c
huiylu <74116599+huiylu@users.noreply.github.com>
2021-04-05 02:14:50 -0700

    Styled brand

--- src/component/Header.js
+++ src/component/Header.js
@@ -3,9 +3,11 @@ function Header(){
         <div className="App-header">
             <img src="./Me.png" alt="Hui Pic" className="img"/>
             <h1>You Hui Lu</h1>
-            <h8 classname="brand">A precise problem solver. Hui is a full stack developer with adaptablity to new technology. Having to learn HTML in merely one day, Hui also brings a passion 
-                to find solutions as well as going through step meticulously to ensure quality. 
-            </h8>
+            <div className="brand">
+                <h8 classname="brand">A precise problem solver. Hui is a full stack developer with adaptablity to new technology. Having to learn HTML in merely one day, Hui also brings a passion 
+                    to find solutions as well as going through step meticulously to ensure quality. 
+                </h8>
+            </div>
         </div>
     )
 
