e57849adf58221dc9702e263d75e47a9db36f809
huiylu <74116599+huiylu@users.noreply.github.com>
2021-04-06 04:08:28 -0700

    added icons

--- src/component/Skills.js
+++ src/component/Skills.js
@@ -1,14 +1,38 @@
 function Skills(){
     return(
-        <div className="info-block">
+        <div className="skill-block" className="info-block">
             <h1>Skills</h1>
             <div className="block">
-                <p>HTML5</p>
-                <p>Javascript</p>
-                <p>Node.js</p>
-                <p>REACT</p>
-                <p>Python</p>
-                <p>MongoDB</p>
+                <div className="skill-block">
+                    <img src="./html5.png" alt="j" className="icon-img"/>
+                    <p>HTML5</p>
+                </div>
+                <div className="skill-block">
+                    <img src="./javascript.png" alt="j" className="icon-img"/>
+                    <p>Javascript</p>
+                </div>
+                <div className="skill-block">
+                    <img src="./nodejs.png" alt="j" className="icon-img"/>
+                    <p>Node.js</p>
+                </div>
+                <div className="skill-block">
+                    <img src="./react.png" alt="j" className="icon-img"/>
+                    <p>REACT</p>
+                </div>
+                <div className="skill-block">
+                    <img src="./python.png" alt="j" className="icon-img"/>
+                    <p>Python</p>
+                </div>
+                <div className="skill-block">
+                    <img src="./mongodb.png" alt="j" className="icon-img"/>
+                    <p>MongoDB</p>
+                </div>
+                
+                
+                
+                
+                
+                
             </div>
             
             
