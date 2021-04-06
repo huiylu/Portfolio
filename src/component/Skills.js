1da03cc1e10ba5c6d2bf61eb1f4910ad94d2bfa0
huiylu <74116599+huiylu@users.noreply.github.com>
2021-04-06 04:13:18 -0700

    added icons

--- src/component/Skills.js
+++ src/component/Skills.js
@@ -1,30 +1,30 @@
 function Skills(){
     return(
-        <div className="skill-block" className="info-block">
+        <div className="info-block">
             <h1>Skills</h1>
             <div className="block">
                 <div className="skill-block">
-                    <img src="./html5.png" alt="j" className="icon-img"/>
+                    <img src="./html5.png" alt="html5" className="icon-img"/>
                     <p>HTML5</p>
                 </div>
                 <div className="skill-block">
-                    <img src="./javascript.png" alt="j" className="icon-img"/>
+                    <img src="./javascript.png" alt="javascript" className="icon-img"/>
                     <p>Javascript</p>
                 </div>
                 <div className="skill-block">
-                    <img src="./nodejs.png" alt="j" className="icon-img"/>
+                    <img src="./nodejs.png" alt="nodejs" className="icon-img"/>
                     <p>Node.js</p>
                 </div>
                 <div className="skill-block">
-                    <img src="./react.png" alt="j" className="icon-img"/>
+                    <img src="./react.png" alt="react" className="icon-img"/>
                     <p>REACT</p>
                 </div>
                 <div className="skill-block">
-                    <img src="./python.png" alt="j" className="icon-img"/>
+                    <img src="./python.png" alt="python" className="icon-img"/>
                     <p>Python</p>
                 </div>
                 <div className="skill-block">
-                    <img src="./mongodb.png" alt="j" className="icon-img"/>
+                    <img src="./mongodb.png" alt="mongodb" className="icon-img"/>
                     <p>MongoDB</p>
                 </div>
                 
