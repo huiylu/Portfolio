2f691d70aa7b2a1559ecb7ba6c832e8e63750ab0
huiylu <74116599+huiylu@users.noreply.github.com>
2021-04-14 16:57:44 -0700

    fixed typo

--- src/component/About.js
+++ src/component/About.js
@@ -8,7 +8,7 @@ function About(){
             </div>
             <div>
                 <h3>About Me</h3>
-                <p>I am a full stack engineer and aviid gamer. Games are my passion and I bring that passion into my code.
+                <p>I am a full stack engineer and avid gamer. Games are my passion and I bring that passion into my code.
                     Like in games, I strive to be adaptable with new technology and try to approach problems in different ways.
                     I try to make sure things are completed to satisfaction and most of all, I try to enjoy what I do.
                 </p>
